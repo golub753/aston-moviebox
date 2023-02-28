7fd1f626179c1e4556034c267b73657bfc00430c
Anton Holub <golubanton753@gmail.com>
2023-02-28 16:50:53 +0300

    feat: apply context

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useContext } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Preloader } from './components/index';
 import { Movie } from './components/Movie/Movie';
@@ -6,13 +6,31 @@ import { Movie } from './components/Movie/Movie';
 const Home = React.lazy(() => import('./pages/Home/Home'));
 const ErrorPage = React.lazy(() => import('./pages/ErrorPage/ErrorPage'));
 
+const themes = {
+ light: {
+  background: '#ffffff',
+ },
+ dark: {
+  background: '#3d3b35',
+ },
+};
+
+export const ThemeContext = React.createContext(themes);
+
 const App = () => {
  return (
   <Router>
    <Suspense fallback={<Preloader />}>
     <Routes>
      <Route path="*" element={<ErrorPage />} />
-     <Route path="/" element={<Home />} />
+     <Route
+      path="/"
+      element={
+       <ThemeContext.Provider value={themes.light}>
+        <Home />
+       </ThemeContext.Provider>
+      }
+     />
      <Route path="/:id" element={<Movie />} />
     </Routes>
    </Suspense>
--- src/components/Movies/Movies.tsx
+++ src/components/Movies/Movies.tsx
@@ -1,19 +1,21 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { fetchMovies } from '../../store/moviesSlice';
 import { useDispatch } from 'react-redux';
 import { Container } from '../index';
 import { MoviesList } from './MoviesList/MoviesList';
 import s from './Movies.module.scss';
+import { ThemeContext } from '../../App';
 
 export const Movies = () => {
  const dispatch = useDispatch();
+ const themes = useContext(ThemeContext);
 
  useEffect(() => {
   dispatch(fetchMovies());
  }, []);
 
  return (
-  <div className={s.movies}>
+  <div className={s.movies} style={themes}>
    <Container>
     <div className={s.movies_title}>Movies</div>
     <MoviesList />
