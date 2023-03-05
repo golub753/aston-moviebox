295a0c439fb3cc61d5042871ac128fc688bc0c3a
Anton Holub <golubanton753@gmail.com>
2023-03-05 20:54:14 +0300

    feat: create hoc error boundary

---
+++ src/hoc/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+type ErrorBoundaryState = {
+ hasError: boolean;
+};
+
+interface ErrorBoundary {
+ props: any;
+ state: ErrorBoundaryState;
+}
+
+class ErrorBoundary extends React.Component<ErrorBoundary> {
+ constructor(props: ErrorBoundary) {
+  super(props);
+  this.state = {
+   hasError: false,
+  };
+ }
+
+ static getDerivedStateFromError(error) {
+  return { hasError: true };
+ }
+
+ render() {
+  if (this.state.hasError) {
+   return <div className="something_wrong">Something went wrong...</div>;
+  }
+  return this.props.children;
+ }
+}
+
+export default ErrorBoundary;
