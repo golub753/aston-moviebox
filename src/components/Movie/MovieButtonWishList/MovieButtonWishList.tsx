4e0683c6610348ca7ed6b67d266f5a89e376187a
Anton Holub <golubanton753@gmail.com>
2023-03-09 19:31:50 +0300

    fix: some fix

--- src/components/Movie/MovieButtonWishList/MovieButtonWishList.tsx
+++ src/components/Movie/MovieButtonWishList/MovieButtonWishList.tsx
@@ -1,44 +1,17 @@
-import { ReactNode, useEffect, useState } from 'react';
-import { useAppSelector, useAppDispatch } from '../../../hooks/hooks';
+import { ReactNode } from 'react';
 import s from './MovieButtonWishList.module.scss';
-import { pushMovie } from '../../../store/wishlistSlice';
-import { pushMovieInUser } from '../../../store/userSlice';
-import { getTokenUser } from '../../../helpers/getTokenUser';
 
 type MovieButtonWishListProps = {
  children: ReactNode;
  props: object;
  id: string | undefined;
+ click: Function;
 };
 
 export const MovieButtonWishList = (props: MovieButtonWishListProps) => {
- const [dataUsers, setDataUsers] = useState([]);
- const user = useAppSelector((state) => state.user.user);
- const movies = useAppSelector((state) => state.movies.movies);
- const wishlist = useAppSelector((state) => state.wishlist.wishlist);
- const dispatch = useAppDispatch();
-
- useEffect(() => {
-  fetch('https://aston-moviebox-default-rtdb.firebaseio.com/users.json')
-   .then((response) => response.json())
-   .then((json) => setDataUsers(json));
- }, []);
-
- const addInWishlist = () => {
-  const movie = movies.find((item) => item.id === +props.id);
-  const inWishlist = wishlist.find((item) => item.id === +movie.id);
-  if (!inWishlist) {
-   const token = getTokenUser(dataUsers, user.mail);
-   dispatch(pushMovie(movie));
-   const moviesArray = [...wishlist, movie];
-   dispatch(pushMovieInUser({ token, moviesArray }));
-  }
- };
  return (
-  user.name && (
-   <button onClick={addInWishlist} className={s.button}>
-    {props.children}
-   </button>
-  )
+  <button className={s.button} onClick={() => props.click()}>
+   {props.children}
+  </button>
  );
 };
--- src/components/Movie/MovieMain/MovieMain.tsx
+++ src/components/Movie/MovieMain/MovieMain.tsx
@@ -1,6 +1,11 @@
 import { Container } from '../../index';
 import s from './MovieMain.module.scss';
+import { useEffect, useState } from 'react';
 import { MovieButtonWishList } from '../MovieButtonWishList/MovieButtonWishList';
+import { useAppSelector, useAppDispatch } from '../../../hooks/hooks';
+import { pushMovie } from '../../../store/wishlistSlice';
+import { pushMovieInUser } from '../../../store/userSlice';
+import { getTokenUser } from '../../../helpers/getTokenUser';
 
 import imdbImage from '../../../assets/imdb.svg';
 
@@ -12,6 +17,28 @@ type MovieMainProps = {
 };
 
 export const MovieMain = ({ img, title, imdb, id }: MovieMainProps) => {
+ const [dataUsers, setDataUsers] = useState([]);
+ const user = useAppSelector((state) => state.user.user);
+ const movies = useAppSelector((state) => state.movies.movies);
+ const wishlist = useAppSelector((state) => state.wishlist.wishlist);
+ const dispatch = useAppDispatch();
+
+ useEffect(() => {
+  fetch('https://aston-moviebox-default-rtdb.firebaseio.com/users.json')
+   .then((response) => response.json())
+   .then((json) => setDataUsers(json));
+ }, []);
+
+ const addInWishlist = () => {
+  const movie = movies.find((item) => item.id === +id);
+  const moviesArray = [...wishlist, movie];
+  const inWishlist = wishlist.find((item) => item.id === +movie.id);
+  if (!inWishlist) {
+   const token = getTokenUser(dataUsers, user.mail);
+   dispatch(pushMovie(movie));
+   dispatch(pushMovieInUser({ token, moviesArray }));
+  }
+ };
  return (
   <div className={s.movie}>
    <img src={img} alt={img} className={s.movie_main} />
@@ -27,7 +54,11 @@ export const MovieMain = ({ img, title, imdb, id }: MovieMainProps) => {
         </div>
        </div>
       )}
-      <MovieButtonWishList id={id}>Add to WishList</MovieButtonWishList>
+      {user.name && (
+       <MovieButtonWishList id={id} click={addInWishlist}>
+        Add to WishList
+       </MovieButtonWishList>
+      )}
      </div>
     </Container>
    </div>
