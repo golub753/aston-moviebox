b4d71fd7dab2a19c1e437dad08d26d2ab0bd4e29
Anton Holub <golubanton753@gmail.com>
2023-03-10 11:00:46 +0300

    feat: create custom middleware

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,17 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { applyMiddleware, createStore, combineReducers } from '@reduxjs/toolkit';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunkMiddleware from 'redux-thunk';
 import moviesReducer from './moviesSlice';
 import popUpReducer from './popUpSlice';
 import userReducer from './userSlice';
 import wishlistReducer from './wishlistSlice';
+import { logger } from '../middleware/logger';
 
-export const store = configureStore({
- reducer: {
-  movies: moviesReducer,
-  popUp: popUpReducer,
-  user: userReducer,
-  wishlist: wishlistReducer,
- },
+const rootReducers = combineReducers({
+ movies: moviesReducer,
+ popUp: popUpReducer,
+ user: userReducer,
+ wishlist: wishlistReducer,
 });
+
+export const store = createStore(rootReducers, composeWithDevTools(applyMiddleware(thunkMiddleware, logger)));
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
