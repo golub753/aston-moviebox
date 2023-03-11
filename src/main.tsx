9d036137b0af743b3f2e608a5b61f30a5fbe4e7b
Anton Holub <golubanton753@gmail.com>
2023-03-11 17:09:57 +0300

    fix: returned strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { store } from './store/store';
@@ -7,7 +8,9 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
- <Provider store={store}>
-  <App />
- </Provider>
+ <React.StrictMode>
+  <Provider store={store}>
+   <App />
+  </Provider>
+ </React.StrictMode>
 );
